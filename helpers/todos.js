1163d647ad75e6a181a0ef98daa381dc85075a0b
Cesar Gomez <zootechdrum@gmail.com>
2019-01-06 21:45:18 -0800

    added helper files/ cleaned up code

---
+++ helpers/todos.js
@@ -0,0 +1,40 @@
+const db = require("../models")
+
+exports.getTodos = (req , res) => {
+    db.Todo.find()
+    .then(todos => {res.json(todos)})
+    .catch(err => {re.send(err)})
+}
+
+exports.getTodo = (req , res) => {
+    db.Todo.findById(req.params.todoId)
+    .then( foundTodo => {res.json(foundTodo)
+    })
+    .catch(err => {res.send(err)})
+}
+
+exports.createTodo = (req , res) => {
+    db.Todo.create(req.body)
+    .then((newTodo) => {
+        res.json(newTodo)
+    })
+    .catch(err => {res.send(err)})
+}
+
+exports.updateTodo = (req , res) => {
+    db.Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new: true}) 
+    .then((todo) =>{
+        res.json(todo);
+    })
+    .catch(err => {res.send(err)})
+}
+exports.deleteTodo = (req , res) => {
+    db.Todo.remove({_id: req.params.todoId})
+    .then( () => {res.json({message: "we deleted it"})
+    })
+    .catch(err => {res.send(err)})
+}
+
+
+
+module.exports = exports;
--- routes/todos.js
+++ routes/todos.js
@@ -1,29 +1,17 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
-const db = require("../models")
+const db = require("../models");
+const helpers = require("../helpers/todos");
 
 
 
-router.get("/", (req, res) => {
-    db.Todo.find()
-        .then(todos => {res.json(todos)})
-        .catch(err => {re.send(err)})
-});
+router.route("/")
+    .get(helpers.getTodos)
+    .post(helpers.createTodo)
 
-
-router.post("/", (req,res) => {
-    db.Todo.create(req.body)
-        .then((newTodo) => {
-            res.json(newTodo)
-        })
-        .catch(err => {res.send(err)})
-});
-
-router.get("/:todoId", (req,res) => {
-    db.Todo.findById(req.params.todoId)
-    .then( foundTodo => {res.json(foundTodo)
-    })
-    .catch(err => {res.send(err)})
-})
+router.route('/:todoId')
+    .get(helpers.getTodo)
+    .put(helpers.updateTodo)
+    .delete(helpers.deleteTodo)
 
 module.exports = router;
