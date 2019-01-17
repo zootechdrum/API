e5a2d6471f709da4289b247748824ea91f9efdd3
Cesar Gomez <zootechdrum@gmail.com>
2019-01-16 20:49:32 -0800

    added post route for new todo

--- public/app.js
+++ public/app.js
@@ -1,14 +1,33 @@
 $(document).ready(function(){
     $.getJSON("/api/todos")
     .then(addTodos)
+
+    ('#todoInput').keypress(function(event){
+      if(event.which === 13) {
+        createTodo();
+      } 
+    })
 });
 
 function addTodos(todos) {
     todos.forEach(function(todo){
-      let newTodo = $('<li class="task">' + todo.name + '</li>');
-      if(todo.completed){
-        newTodo.addClass("done");
-      }
-      $('.list').append(newTodo);
+    addTodo(todo);
+
     })
 };
+
+function addTodo(todo){
+  let newTodo = $('<li class="task">' + todo.name + '</li>');
+  if(todo.completed){
+    newTodo.addClass("done");
+  }
+}
+$('.list').append(newTodo);
+function createTodo(){
+  //send request to create new todo
+  let userInput = $('#todoInput').val();
+  $.post('/api/todos',{name:userInput})
+  .then(function(newTodo){
+    console.log(newTodo)
+  })
+}
